c4f90776f181ebc16b4f05ff3be4e668c8330758
Diego Urrego <diego.urrego@sofka.com.co>
2022-11-22 00:26:38 -0500

    Instalación de paquete de iconos IonIcons en Android y prueba de visualización.

--- src/navigator/MaterialBottomTab.tsx
+++ src/navigator/MaterialBottomTab.tsx
@@ -3,15 +3,51 @@ import {createMaterialBottomTabNavigator} from '@react-navigation/material-botto
 import {AccountScreen} from '../screens/tabs/AccountScreen';
 import {LoansScreen} from '../screens/tabs/LoansScreen';
 import {PaymentScreen} from '../screens/tabs/PaymentScreen';
+import {Text} from 'react-native';
 
 const Tab = createMaterialBottomTabNavigator();
 
 export const MaterialBottomTabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Account" component={AccountScreen} />
-      <Tab.Screen name="Loans" component={LoansScreen} />
-      <Tab.Screen name="Payment" component={PaymentScreen} />
+    <Tab.Navigator
+      sceneAnimationEnabled={true}
+      screenOptions={({route}) => ({
+        tabBarLabelStyle: {
+          fontSize: 15,
+        },
+        tabBarIcon: ({color}) => {
+          let iconName: string = '';
+          switch (route.name) {
+            case 'AccountScreen':
+              iconName = 'AC';
+              break;
+
+            case 'LoansScreen':
+              iconName = 'LN';
+              break;
+
+            case 'PaymentScreen':
+              iconName = 'PM';
+              break;
+          }
+          return <Text style={{color}}>{iconName}</Text>;
+        },
+      })}>
+      <Tab.Screen
+        name="AccountScreen"
+        options={{title: 'Account'}}
+        component={AccountScreen}
+      />
+      <Tab.Screen
+        name="LoansScreen"
+        options={{title: 'Loans'}}
+        component={LoansScreen}
+      />
+      <Tab.Screen
+        name="PaymentScreen"
+        options={{title: 'Payment'}}
+        component={PaymentScreen}
+      />
     </Tab.Navigator>
   );
 };
--- src/screens/tabs/AccountScreen.tsx
+++ src/screens/tabs/AccountScreen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {Text, View} from 'react-native';
+import {styles} from '../../theme/appTheme';
 
 export const AccountScreen = () => {
   return (
-    <View>
-      <Text>AccountScreen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Iconos</Text>
+      <Icon name="airplane-outline" size={30} color="#900" />
     </View>
   );
 };
