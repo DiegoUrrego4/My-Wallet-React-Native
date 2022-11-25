7f225476a08c50867879c3ae336d4e93e457de70
Diego Urrego <diego.urrego@sofka.com.co>
2022-11-25 02:45:05 -0500

    estilización pantalla cambiar tema de aplicación completada.

--- src/screens/sidemenu/ChangeAppThemeScreen.tsx
+++ src/screens/sidemenu/ChangeAppThemeScreen.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react';
-import {View, Text, BackHandler} from 'react-native';
+import {View, Text, BackHandler, TouchableOpacity} from 'react-native';
+import {ThemeCard} from '../../components/ThemeCard';
 import {MyStackScreenProps} from '../../interfaces/MyStackScreenProps';
+import {changeThemeStyles} from '../../theme/changeThemeTheme';
 
 export const ChangeAppThemeScreen = ({navigation}: MyStackScreenProps) => {
   useEffect(() => {
@@ -23,8 +25,45 @@ export const ChangeAppThemeScreen = ({navigation}: MyStackScreenProps) => {
     return () => backHandler.remove();
   }, [navigation]);
   return (
-    <View>
-      <Text>ChangeAppThemeScreen</Text>
+    <View style={changeThemeStyles.mainContainer}>
+      <View style={changeThemeStyles.textContainer}>
+        <Text style={changeThemeStyles.title}>Choose your theme</Text>
+        <Text style={changeThemeStyles.paragraph}>
+          You can always change this at any time
+        </Text>
+      </View>
+      <View style={changeThemeStyles.themesContainer}>
+        {/* Fila 1 */}
+        <View style={changeThemeStyles.row}>
+          <ThemeCard title="Arts & Culture" />
+          <ThemeCard title="Arts & Culture" />
+          <ThemeCard title="Arts & Culture" />
+        </View>
+        {/* Fila 2 */}
+        <View style={changeThemeStyles.row}>
+          <ThemeCard title="Arts & Culture" />
+          <ThemeCard title="Arts & Culture" />
+          <ThemeCard title="Arts & Culture" />
+        </View>
+        {/* Fila 3 */}
+        <View style={changeThemeStyles.row}>
+          <ThemeCard title="Arts & Culture" />
+          <ThemeCard title="Arts & Culture" />
+          <ThemeCard title="Arts & Culture" />
+        </View>
+      </View>
+      <View style={changeThemeStyles.buttonsContainer}>
+        <TouchableOpacity style={changeThemeStyles.cancelButton}>
+          <Text
+            style={changeThemeStyles.cancelButtonText}
+            onPress={() => navigation.navigate('MaterialBottomTabs')}>
+            CANCEL
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={changeThemeStyles.button}>
+          <Text style={changeThemeStyles.buttonText}>ACCEPT</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
