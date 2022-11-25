d6045a2407a371a0279059da01302dc9c7b686eb
Diego Urrego <diego.urrego@sofka.com.co>
2022-11-25 00:44:09 -0500

    Se agrega logo al menú lateral y se agregan los respectivos estilos.

--- src/screens/sidemenu/InternalSideMenu.tsx
+++ src/screens/sidemenu/InternalSideMenu.tsx
@@ -16,63 +16,75 @@ export const InternalSideMenu = ({
   };
   return (
     <>
-      {/* Conatiner Avatar */}
-      <View style={sideMenuStyles.avatarContainer}>
-        <Image
-          source={{
-            uri: 'https://avatarairlines.com/wp-content/uploads/2020/05/Male-placeholder.jpeg',
-          }}
-          style={sideMenuStyles.avatar}
-        />
-        {/* <Image
+      <View style={sideMenuStyles.userOptionsContainer}>
+        {/* Conatiner Avatar */}
+        <View style={sideMenuStyles.avatarContainer}>
+          <Image
+            source={{
+              uri: 'https://avatarairlines.com/wp-content/uploads/2020/05/Male-placeholder.jpeg',
+            }}
+            style={sideMenuStyles.avatar}
+          />
+          {/* <Image
       source={require('../../assets/avatar.png')}
       style={sideMenuStyles.avatar}
     /> */}
-        <Text style={sideMenuStyles.username}>User Name</Text>
+          <Text style={sideMenuStyles.username}>User Name</Text>
+        </View>
+        {/* Opciones de Menú */}
+        <View style={sideMenuStyles.menuContainer}>
+          <TouchableOpacity
+            style={sideMenuStyles.menuButton}
+            onPress={() => navigation.navigate('MaterialBottomTabs')}>
+            <Icon
+              style={sideMenuStyles.menuIcon}
+              name="wallet-sharp"
+              size={25}
+            />
+            <Text style={sideMenuStyles.menuText}>My Wallet</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={sideMenuStyles.menuButton}
+            onPress={() => navigation.navigate('ChangePasswordScreen')}>
+            <Icon
+              style={sideMenuStyles.menuIcon}
+              name="lock-closed-sharp"
+              size={25}
+            />
+            <Text style={sideMenuStyles.menuText}>Change Password</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={sideMenuStyles.menuButton}
+            onPress={() => navigation.navigate('ChangeAppThemeScreen')}>
+            <Icon
+              style={sideMenuStyles.menuIcon}
+              name="color-palette-sharp"
+              size={25}
+            />
+            <Text style={sideMenuStyles.menuText}>Change App Theme</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={sideMenuStyles.menuButton}
+            onPress={() => console.log('LOGOUT')}>
+            <Icon
+              style={sideMenuStyles.menuIcon}
+              name="log-out-sharp"
+              size={25}
+            />
+            <Text style={sideMenuStyles.menuText} onPress={handleLogout}>
+              Logout
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      {/* Opciones de Menú */}
-      <View style={sideMenuStyles.menuContainer}>
-        <TouchableOpacity
-          style={sideMenuStyles.menuButton}
-          onPress={() => navigation.navigate('MaterialBottomTabs')}>
-          <Icon style={sideMenuStyles.menuIcon} name="wallet-sharp" size={25} />
-          <Text style={sideMenuStyles.menuText}>My Wallet</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={sideMenuStyles.menuButton}
-          onPress={() => navigation.navigate('ChangePasswordScreen')}>
-          <Icon
-            style={sideMenuStyles.menuIcon}
-            name="lock-closed-sharp"
-            size={25}
-          />
-          <Text style={sideMenuStyles.menuText}>Change Password</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={sideMenuStyles.menuButton}
-          onPress={() => navigation.navigate('ChangeAppThemeScreen')}>
-          <Icon
-            style={sideMenuStyles.menuIcon}
-            name="color-palette-sharp"
-            size={25}
-          />
-          <Text style={sideMenuStyles.menuText}>Change App Theme</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={sideMenuStyles.menuButton}
-          onPress={() => console.log('LOGOUT')}>
-          <Icon
-            style={sideMenuStyles.menuIcon}
-            name="log-out-sharp"
-            size={25}
-          />
-          <Text style={sideMenuStyles.menuText} onPress={handleLogout}>
-            Logout
-          </Text>
-        </TouchableOpacity>
+      <View style={sideMenuStyles.logoContainer}>
+        <Image
+          style={sideMenuStyles.appLogo}
+          source={require('../../../assets/MUIIcon.png')}
+        />
       </View>
     </>
   );
